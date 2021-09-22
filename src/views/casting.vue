2f4c5ed5c9c99556ef4648fda6f296c498a363ac
tst <streicher.tim@googlemail.com>
2021-09-22 22:05:08 +0200

    added content for casting

---
+++ src/views/casting.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col bg-primebot-gray justify-center items-center gap-3 py-10">
+    <div class="w-full ">
+      <img
+          class="max-h-min lg:w-1/2 m-auto"
+          alt="streamcast network logo"
+          src="@/assets/images/streamcast.png">
+
+    </div>
+    <span class="text-white text-center text-2xl lg:w-2/3">
+      Ihr wollt die echte E-Sport Atmosphäre fühlen? Dann lasst eure Spiele vom StreamCastNetwork casten! Egal ob Scrim, PrimeLeague, Uniliga oder euer Turnier.
+    </span>
+  </div>
+
+</template>
